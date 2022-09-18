58131dd271664d547e77bcbb06cfe49de0f3ad96
danieldavid0901@gmail.com <60299227+Daniul@users.noreply.github.com>
2022-09-18 17:04:29 -0500

    added seed files, completed ActiveRecord/Record tasks; finished Lab 4

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Book.create!([
+  {title: "dfd", author: "dfd", price: "23.0", published_date: "2022-09-20"},
+  {title: "red2", author: "johnny", price: "23232.0", published_date: "2022-09-05"},
+  {title: "cyberpunk", author: "lol", price: "23.0", published_date: "2022-08-29"}
+])
+User.create!([
+  {username: "red"},
+  {username: "gigem"},
+  {username: "rev"}
+])
+UserBook.create!([
+  {user_id: 1, book_id: 2},
+  {user_id: 2, book_id: 1},
+  {user_id: 3, book_id: 3}
+])
